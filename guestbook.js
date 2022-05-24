4eab3ada2bcb6a1aa88eba53412af08fad26ed0a
JorvikPudding <102742228+JorvikPudding@users.noreply.github.com>
2022-05-24 01:05:16 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://c93efb30.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
